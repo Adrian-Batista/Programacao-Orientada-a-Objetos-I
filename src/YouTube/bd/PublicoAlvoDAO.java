4521a64ed45b6a8bee469e9b8d3b934c1fd33efe
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2020-09-11 20:48:24 -0300

    Criação da classe PublicoAlvoDAO

---
+++ src/YouTube/bd/PublicoAlvoDAO.java
@@ -0,0 +1,27 @@
+package YouTube.bd;
+
+import java.util.List;
+
+import YouTube.entidades.PublicoAlvo;
+
+public class PublicoAlvoDAO implements InterfaceDAO<PublicoAlvo> {
+
+	@Override
+	public void adicionar(PublicoAlvo referencia) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remover(PublicoAlvo referencia) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<PublicoAlvo> todos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/YouTube/bd/UtilBD.java
+++ src/YouTube/bd/UtilBD.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+//import steam.ihc.AlertaFX;
 
 public class UtilBD {
 	private static Connection conexao;
