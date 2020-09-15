2297a53a2ce6bce314c4c3c92b1a80f96f89041c
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2020-09-15 16:12:19 -0300

    Update UsuarioDAO.java

--- src/YouTube/bd/UsuarioDAO.java
+++ src/YouTube/bd/UsuarioDAO.java
@@ -1,26 +1,72 @@
 package YouTube.bd;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
 import YouTube.entidades.Usuario;
+
 
 public class UsuarioDAO implements InterfaceDAO<Usuario> {
 
-	@Override
 	public void adicionar(Usuario referencia) {
-		// TODO Auto-generated method stub
-		
+		try {
+			String sql = "INSERT INTO Jogador VALUES ('" + referencia.getNome() + "','" + referencia.getEmail() + "','"
+					+ referencia.getSenha() + "')";
+			UtilBD.alterarBD(sql);
+		} catch (SQLException e) {
+			//AlertaFX.erro("Não foi possível inserir o jogador no banco!");
+			System.out.println("Não foi possível inserir o jogador no banco!");
+		}
 	}
 
-	@Override
 	public void remover(Usuario referencia) {
-		// TODO Auto-generated method stub
-		
+		try {
+			String sql = "DELETE FROM Jogador WHERE nome = '" + referencia.getNome() + "'";
+			UtilBD.alterarBD(sql);
+		} catch (SQLException e) {
+			//AlertaFX.erro("Não foi possível remover o jogador do banco!");
+			System.out.println("Não foi possível remover o jogador do banco!");
+		}
 	}
 
-	@Override
 	public List<Usuario> todos() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Usuario> retorno = new ArrayList<Usuario>();
+		try {
+			String sql = "SELECT Nome, Email, Senha, Apelido FROM Jogador";
+			ResultSet resultSet = UtilBD.consultarBD(sql);
+			while (resultSet.next()) {
+				String nome = resultSet.getString("Nome");
+				String email = resultSet.getString("Email");
+				String senha = resultSet.getString("Senha");
+				retorno.add(new Usuario(nome, email, senha));
+			}
+			resultSet.getStatement().close();
+		} catch (SQLException e) {
+			//AlertaFX.erro("Não foi possível consultar todos os jogadores do banco!");
+			System.out.println("Não foi possível consultar todos os jogadores do banco!");
+		}
+		return retorno;
+	}
+
+	public Usuario get(String nome) {
+		Usuario retorno = null;
+		try {
+			String sql = "SELECT Email, Senha, Apelido FROM Jogador WHERE Nome = '" + nome + "'";
+			ResultSet resultSet = UtilBD.consultarBD(sql);
+			while (resultSet.next()) {
+				String email = resultSet.getString("Email");
+				String senha = resultSet.getString("Senha");
+				retorno = new Usuario(nome, email, senha);
+			}
+			// PRECISO FECHAR O STATEMENT SÓ DEPOIS!
+			resultSet.getStatement().close();
+		} catch (SQLException e) {
+			//AlertaFX.erro("Não foi possível consultar um jogador do banco!");
+			System.out.println("Não foi possível consultar um jogador do banco!");
+		}
+		return retorno;
 	}
 
 }
